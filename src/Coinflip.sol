282d3afedad9e1e496b32ffcfa67d4e28c9b4eea
Tiny-boot <kathleenrrogan@gmail.com>
2025-02-01 12:30:18 +0100

    mod

--- src/Coinflip.sol
+++ src/Coinflip.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.28;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-
+//WARNING: NEED TO ADD A DEPENDENCY INTO FORGE
 error SeedTooShort();
 
 /// @title Coinflip 10 in a Row
@@ -13,54 +13,56 @@ contract Coinflip is Ownable{
     string public seed;
 
     constructor() Ownable(msg.sender) {
-        // TODO: set the seed to "It is a good practice to rotate seeds often in gambling".
+        seed = "It is a good practice to rotate seeds often in gambling";
     }
 
     /// @notice Checks user input against contract generated guesses
     /// @param Guesses is a fixed array of 10 elements which holds the user's guesses. The guesses are either 1 or 0 for heads or tails
     /// @return true if user correctly guesses each flip correctly or false otherwise
     function userInput(uint8[10] calldata Guesses) external view returns(bool){
-        // TODO: Get the contract generated flips by calling the helper function getFlips()
-
-        // TODO: Compare each element of the user's guesses with the generated guesses. Return true ONLY if all guesses match
-
+        uint8[10] memory flips = getFlips();
+        for (uint i = 0; i < 10; i++){
+            if (Guesses[i] != flips[i]){
+                return false;
+            }
+        }
+        return true;
     }
 
     /// @notice allows the owner of the contract to change the seed to a new one
     /// @param NewSeed is a string which represents the new seed
     function seedRotation(string memory NewSeed) public onlyOwner {
-        // TODO: Cast the string into a bytes array so we may perform operations on it
-
-        // TODO: Get the length of the array (ie. how many characters in this string)
-
+        bytes memory stringInBytes = bytes(NewSeed);
+        uint seedlength = stringInBytes.length;
         // Check if the seed is less than 10 characters (This function is given to you)
         if (seedlength < 10){
             revert SeedTooShort();
         }
         
-        //TODO: Set the seed variable as the NewSeed
-
+        seed = NewSeed;
     }
 
 // -------------------- helper functions -------------------- //
     /// @notice This function generates 10 random flips by hashing characters of the seed
     /// @return a fixed 10 element array of type uint8 with only 1 or 0 as its elements
-    function getFlips() public view returns(uint8[10] memory){
-        // TODO: Cast the seed into a bytes array and get its length
+function getFlips() public view returns(uint8[10] memory){
+    bytes memory stringInBytes = bytes(seed);
+    uint seedlength = stringInBytes.length;
+    uint8[10] memory elements;
+    // Setting the interval for grabbing characters
+    uint interval = seedlength / 10;
 
-        // TODO: Initialize an empty fixed array with 10 uint8 elements
-
-        // Setting the interval for grabbing characters
-        uint interval = seedlength / 10;
-
-        // TODO: Input the correct form for the for loop
-        for (){
-            // Generating a pseudo-random number by hashing together the character and the block timestamp
-            uint randomNum = uint(keccak256(abi.encode(stringInBytes[i*interval], block.timestamp)));
-            
-            // TODO: if the result is an even unsigned integer, record it as 1 in the results array, otherwise record it as zero
+    for (uint i = 0; i < 10; i++){
+        // Generating a pseudo-random number by hashing together the character and the block timestamp
+        uint randomNum = uint(keccak256(abi.encode(stringInBytes[i*interval % seedlength], block.timestamp)));
+        
+        if (randomNum % 2 == 0){
+            elements[i] = 1;
+        } else {
+            elements[i] = 0;
         }
-
-        //TODO: return the resulting fixed array
     }
+
+    return elements;
+}
 }
